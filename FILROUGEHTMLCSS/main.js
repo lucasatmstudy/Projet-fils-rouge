f584368ac956a3fda82be7dad60873a6682970f0
lucasatmstudy <lucasinfostudy@gmail.com>
2026-05-26 08:00:55 +0200

    MAJ 25 05 2026

--- FILROUGEHTMLCSS/main.js
+++ FILROUGEHTMLCSS/main.js
@@ -1,8 +1,14 @@
 window.addEventListener('scroll', () => {
-    const header = document.querySelector('Header');
+    const header = document.querySelector('header');
     if (window.scrollY > 0) {
         header.classList.add('scrolled');
     } else {
         header.classList.remove('scrolled');
+    }
+});
+
+document.querySelectorAll('*').forEach(el => {
+    if (el.offsetWidth > document.documentElement.offsetWidth) {
+        console.log(el);
     }
 });
